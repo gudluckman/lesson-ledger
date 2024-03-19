3597007699a2f94351e1427eacb2213575cf08a9
Edward Lukman <gudluckman@github.com>
2024-03-19 23:49:33 +1100

    fixed with caching from last production and optimized fetching

--- client/src/pages/LessonSchedule/lesson-schedule.tsx
+++ client/src/pages/LessonSchedule/lesson-schedule.tsx
@@ -27,18 +27,12 @@ const LessonSchedule: React.FC = () => {
 
   const fetchEvents = useCallback(async () => {
     try {
-      const cachedEvents = localStorage.getItem("cachedEvents");
-      if (cachedEvents) {
-        setEvents(JSON.parse(cachedEvents));
-        setLoading(false);
-      } else {
-        const response = await axios.get(
-          "https://lesson-ledger.onrender.com/api/v1/lessons/events"
-        );
-        setEvents(response.data);
-        localStorage.setItem("cachedEvents", JSON.stringify(response.data));
-        setLoading(false);
-      }
+      const response = await axios.get(
+        // "https://lesson-ledger.onrender.com/api/v1/lessons/events"
+        "http://localhost:5005/api/v1/lessons/events"
+      );
+      setEvents(response.data);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching events:", error);
       setLoading(false);
@@ -53,15 +47,15 @@ const LessonSchedule: React.FC = () => {
   useEffect(() => {
     let totalSum = 0;
     let totalHours = 0;
-    let eventCount = 0;
+    const studentsSet = new Set();
 
     events.forEach((event: any) => {
-      const regex = /\$(\d+)/g;
-      const matches = event.description.match(regex);
-      if (matches) {
-        matches.forEach((match: any) => {
-          totalSum += parseInt(match.substring(1));
-        });
+      const nameRegex = /^(.+)\s+\(/; // Extract name of student
+      const matches = event.summary.match(nameRegex);
+      if (matches && matches.length > 1) {
+        const studentName = matches[1].trim();
+        // Add the studentName to the Set
+        studentsSet.add(studentName);
       }
 
       // Calculate duration in hours for each event
@@ -70,11 +64,22 @@ const LessonSchedule: React.FC = () => {
       const durationInMs = end.getTime() - start.getTime();
       const durationInHours = durationInMs / (1000 * 60 * 60);
       totalHours += durationInHours;
-
-      // Calculate each event
-      eventCount++;
     });
-    // averageHourlyRate = totalSum / totalHours;
+
+    // Get the size of the Set to get unique student count
+    const eventCount = studentsSet.size;
+
+    // Calculate total sum and average hourly rate
+    totalSum = events.reduce((acc: number, event: any) => {
+      const regex = /\$(\d+)/g;
+      const matches = event.description.match(regex);
+      if (matches) {
+        matches.forEach((match: any) => {
+          acc += parseInt(match.substring(1));
+        });
+      }
+      return acc;
+    }, 0);
 
     localStorage.setItem("currentWeeklyIncomeSum", totalSum.toString());
     localStorage.setItem("currentWeeklyHours", totalHours.toString());
@@ -114,193 +119,202 @@ const LessonSchedule: React.FC = () => {
   };
 
   return (
-    <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+    <Box>
       <Helmet>
         <title>Lesson Schedule</title>
       </Helmet>
-      <Stack direction="column" gap={3}>
-        <Typography fontSize={25} fontWeight={700} color="#11142d">
-          This Week's Lessons
-        </Typography>
-        <CustomButton
-          title="Edit Calendar"
-          handleClick={() =>
-            window.open("https://calendar.google.com/calendar/u/0/r", "_blank")
-          }
-          backgroundColor="#475be8"
-          color="#fcfcfc"
-          icon={<EditIcon />}
-        />
-        {loading && (
-          <Typography>Please wait a moment while fetching...</Typography>
-        )}
-      </Stack>
-      {!loading && (
-        <Grid container spacing={2}>
-          {[
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-          ].map((day, index) => (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              md={4}
-              lg={4}
-              xl={2}
-              key={day}
-              sx={{ borderRight: index !== 6 ? "1px solid #ccc" : "none" }}
-            >
-              <List>
-                <Typography
-                  variant="h5"
-                  gutterBottom
-                  style={{
-                    color: currentDay === index ? "#475BE8" : "inherit",
-                    fontWeight: 600,
-                    marginBottom: "8px",
-                  }}
-                >
-                  {day}
-                </Typography>
-                {filterEventsByDay(day).length === 0 ? (
-                  <ListItem>
-                    <Typography
-                      variant="body1"
-                      color="textSecondary"
-                      sx={{
-                        py: 1,
-                        ml: -2,
-                      }}
-                    >
-                      No Lesson Scheduled
-                    </Typography>
-                  </ListItem>
-                ) : (
-                  filterEventsByDay(day).map((event: any) => (
-                    <ListItem
-                      key={event.id}
-                      sx={{
-                        py: 1,
-                        ml: -2,
-                      }}
-                    >
-                      <Card
-                        variant="outlined"
-                        style={{
-                          width: "100%",
-                          backgroundColor:
-                            currentDay === index ? "#475BE8" : "#fff",
-                          color: currentDay === index ? "#fff" : "inherit",
+      <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+        <Stack direction="column" gap={3}>
+          <Typography fontSize={25} fontWeight={700} color="#11142d">
+            This Week's Lessons
+          </Typography>
+          <CustomButton
+            title="Edit Calendar"
+            handleClick={() =>
+              window.open(
+                "https://calendar.google.com/calendar/u/0/r",
+                "_blank"
+              )
+            }
+            backgroundColor="#475be8"
+            color="#fcfcfc"
+            icon={<EditIcon />}
+          />
+          {loading && (
+            <Typography>Please wait a moment while fetching...</Typography>
+          )}
+        </Stack>
+        {!loading && (
+          <Grid container spacing={2}>
+            {[
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday",
+              "Saturday",
+              "Sunday",
+            ].map((day, index) => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={4}
+                lg={4}
+                xl={2}
+                key={day}
+                sx={{ borderRight: index !== 6 ? "1px solid #ccc" : "none" }}
+              >
+                <List>
+                  <Typography
+                    variant="h5"
+                    gutterBottom
+                    style={{
+                      color: currentDay === index ? "#475BE8" : "inherit",
+                      fontWeight: 600,
+                      marginBottom: "8px",
+                    }}
+                  >
+                    {day}
+                  </Typography>
+                  {filterEventsByDay(day).length === 0 ? (
+                    <ListItem>
+                      <Typography
+                        variant="body1"
+                        color="textSecondary"
+                        sx={{
+                          py: 1,
+                          ml: -2,
                         }}
                       >
-                        <CardContent>
-                          <Typography variant="subtitle1" gutterBottom>
-                            {event.summary}
-                          </Typography>
-                          <div
-                            style={{
-                              display: "flex",
-                              alignItems: "center",
-                              marginTop: "8px",
-                            }}
-                          >
-                            <AccessTimeFilledIcon
-                              style={{ marginRight: "8px" }}
-                            />
-                            <Typography
-                              variant="body2"
-                              style={{
-                                color:
-                                  currentDay === index ? "white" : "inherit",
-                              }}
-                            >
-                              {formatTime(event.start.dateTime)} to{" "}
-                              {formatTime(event.end.dateTime)}
-                            </Typography>
-                          </div>
-                          <div
-                            style={{
-                              display: "flex",
-                              alignItems: "center",
-                              marginTop: "8px",
-                            }}
-                          >
-                            <PaidIcon style={{ marginRight: "8px" }} />
-                            <Typography
-                              variant="body2"
-                              style={{
-                                color:
-                                  currentDay === index ? "white" : "inherit",
-                              }}
-                            >
-                              {event.description}
-                            </Typography>
-                          </div>
-                          <div
-                            style={{
-                              display: "flex",
-                              alignItems: "center",
-                              marginTop: "8px",
-                            }}
-                          >
-                            <VideocamIcon style={{ marginRight: "8px" }} />
-                            <Typography
-                              variant="body2"
-                              style={{
-                                color:
-                                  currentDay === index ? "#FFFFFF" : "#1976D2",
-                                textDecoration: "none",
-                                cursor: "pointer",
-                              }}
-                              onClick={() =>
-                                window.open(event.hangoutLink, "_blank")
-                              }
-                            >
-                              Meeting
-                            </Typography>
-                          </div>
-                          <div
-                            style={{
-                              display: "flex",
-                              alignItems: "center",
-                              marginTop: "8px",
-                            }}
-                          >
-                            <ModeEditIcon style={{ marginRight: "8px" }} />
-                            <Typography
-                              variant="body2"
-                              style={{
-                                color:
-                                  currentDay === index ? "#FFFFFF" : "#1976D2",
-                                textDecoration: "none",
-                                cursor: "pointer",
-                              }}
-                              onClick={() =>
-                                window.open(
-                                  event.attachments[0].fileUrl,
-                                  "_blank"
-                                )
-                              }
-                            >
-                              Jamboard
-                            </Typography>
-                          </div>
-                        </CardContent>
-                      </Card>
+                        No Lesson Scheduled
+                      </Typography>
                     </ListItem>
-                  ))
-                )}
-              </List>
-            </Grid>
-          ))}
-        </Grid>
-      )}
+                  ) : (
+                    filterEventsByDay(day).map((event: any) => (
+                      <ListItem
+                        key={event.id}
+                        sx={{
+                          py: 1,
+                          ml: -2,
+                        }}
+                      >
+                        <Card
+                          variant="outlined"
+                          style={{
+                            width: "100%",
+                            backgroundColor:
+                              currentDay === index ? "#475BE8" : "#fff",
+                            color: currentDay === index ? "#fff" : "inherit",
+                          }}
+                        >
+                          <CardContent>
+                            <Typography variant="subtitle1" gutterBottom>
+                              {event.summary}
+                            </Typography>
+                            <div
+                              style={{
+                                display: "flex",
+                                alignItems: "center",
+                                marginTop: "8px",
+                              }}
+                            >
+                              <AccessTimeFilledIcon
+                                style={{ marginRight: "8px" }}
+                              />
+                              <Typography
+                                variant="body2"
+                                style={{
+                                  color:
+                                    currentDay === index ? "white" : "inherit",
+                                }}
+                              >
+                                {formatTime(event.start.dateTime)} to{" "}
+                                {formatTime(event.end.dateTime)}
+                              </Typography>
+                            </div>
+                            <div
+                              style={{
+                                display: "flex",
+                                alignItems: "center",
+                                marginTop: "8px",
+                              }}
+                            >
+                              <PaidIcon style={{ marginRight: "8px" }} />
+                              <Typography
+                                variant="body2"
+                                style={{
+                                  color:
+                                    currentDay === index ? "white" : "inherit",
+                                }}
+                              >
+                                {event.description}
+                              </Typography>
+                            </div>
+                            <div
+                              style={{
+                                display: "flex",
+                                alignItems: "center",
+                                marginTop: "8px",
+                              }}
+                            >
+                              <VideocamIcon style={{ marginRight: "8px" }} />
+                              <Typography
+                                variant="body2"
+                                style={{
+                                  color:
+                                    currentDay === index
+                                      ? "#FFFFFF"
+                                      : "#1976D2",
+                                  textDecoration: "none",
+                                  cursor: "pointer",
+                                }}
+                                onClick={() =>
+                                  window.open(event.hangoutLink, "_blank")
+                                }
+                              >
+                                Meeting
+                              </Typography>
+                            </div>
+                            <div
+                              style={{
+                                display: "flex",
+                                alignItems: "center",
+                                marginTop: "8px",
+                              }}
+                            >
+                              <ModeEditIcon style={{ marginRight: "8px" }} />
+                              <Typography
+                                variant="body2"
+                                style={{
+                                  color:
+                                    currentDay === index
+                                      ? "#FFFFFF"
+                                      : "#1976D2",
+                                  textDecoration: "none",
+                                  cursor: "pointer",
+                                }}
+                                onClick={() =>
+                                  window.open(
+                                    event.attachments[0].fileUrl,
+                                    "_blank"
+                                  )
+                                }
+                              >
+                                Jamboard
+                              </Typography>
+                            </div>
+                          </CardContent>
+                        </Card>
+                      </ListItem>
+                    ))
+                  )}
+                </List>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
     </Box>
   );
 };
