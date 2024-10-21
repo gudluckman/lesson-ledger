59b5d3f4acc58682da0e294ad05739dd17659bc1
gudluckman <edwlaw12@gmail.com>
2024-10-21 12:51:27 +1100

    added api endpoints for next week lesson fetching from calendar

--- server/controllers/calendar.controller.js
+++ server/controllers/calendar.controller.js
@@ -4,8 +4,8 @@ import { google } from "googleapis";
 const jwtClient = new google.auth.JWT({
   email: process.env.CLIENT_EMAIL,
   keyFile: "./service_account.json",
-  key: process.env.PRIVATE_KEY.split(String.raw`\n`).join("\n"),
-  // key: process.env.PRIVATE_KEY, // Uncomment this for testing
+  // key: process.env.PRIVATE_KEY.split(String.raw`\n`).join("\n"),
+  key: process.env.PRIVATE_KEY, // Uncomment this for testing
   scopes: ["https://www.googleapis.com/auth/calendar"],
 });
 
@@ -35,12 +35,12 @@ const fetchWeeklyEvents = async (req, res) => {
       endOfWeek.setHours(23, 59, 59, 999); // Set to 23:59:59
     }
 
-    // console.log(
-    //   `Start of the week: ${startOfWeek.toDateString()} ${startOfWeek.toTimeString()}`
-    // );
-    // console.log(
-    //   `End of the week: ${endOfWeek.toDateString()} ${endOfWeek.toTimeString()}`
-    // );
+    console.log(
+      `Start of the week: ${startOfWeek.toDateString()} ${startOfWeek.toTimeString()}`
+    );
+    console.log(
+      `End of the week: ${endOfWeek.toDateString()} ${endOfWeek.toTimeString()}`
+    );
 
     const response = await calendar.events.list({
       calendarId: process.env.CALENDAR_ID,
@@ -63,4 +63,54 @@ const fetchWeeklyEvents = async (req, res) => {
   }
 };
 
-export { fetchWeeklyEvents };
+const fetchNextWeeklyEvents = async (req, res) => {
+  try {
+    const today = new Date();
+    const isSunday = today.getDay() === 0;
+    let startOfNextWeek;
+    let endOfNextWeek;
+
+    if (isSunday) {
+      startOfNextWeek = new Date(today);
+      startOfNextWeek.setDate(today.getDate() - 6);
+      startOfNextWeek.setHours(0, 0, 0, 0);
+      endOfNextWeek = new Date(today);
+      endOfNextWeek.setHours(23, 59, 59, 999);
+    } else {
+      startOfNextWeek = new Date(today);
+      startOfNextWeek.setDate(today.getDate() - today.getDay() + 8); // Go back to Monday of the next week
+      startOfNextWeek.setHours(0, 0, 0, 0); // Set to 00:00:00
+      endOfNextWeek = new Date(today);
+      endOfNextWeek.setDate(today.getDate() + (14 - today.getDay())); // Go forward to Sunday of the next week
+      endOfNextWeek.setHours(23, 59, 59, 999); // Set to 23:59:59
+    }
+
+    console.log(
+      `Start of the next week: ${startOfNextWeek.toDateString()} ${startOfNextWeek.toTimeString()}`
+    );
+    console.log(
+      `End of the next week: ${endOfNextWeek.toDateString()} ${endOfNextWeek.toTimeString()}`
+    );
+
+    const response = await calendar.events.list({
+      calendarId: process.env.CALENDAR_ID,
+      timeMin: startOfNextWeek.toISOString(),
+      timeMax: endOfNextWeek.toISOString(),
+      maxResults: 50,
+      singleEvents: true,
+      orderBy: "startTime",
+      fields: "items(colorId,summary,start,end,hangoutLink,description,attachments)",
+    });
+
+    const events = response.data.items.filter(
+      (event) => event.colorId === "1" || event.colorId === "9"
+    );
+
+    res.json(events);
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export { fetchWeeklyEvents, fetchNextWeeklyEvents };
--- server/routes/calendar.routes.js
+++ server/routes/calendar.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { fetchWeeklyEvents } from "../controllers/calendar.controller.js";
+import { fetchNextWeeklyEvents, fetchWeeklyEvents } from "../controllers/calendar.controller.js";
 
 const router = express.Router();
 
 // Route to fetch weekly events from Google Calendar
 router.get("/events", fetchWeeklyEvents);
+router.get("/future-events", fetchNextWeeklyEvents);
 
 export default router;
